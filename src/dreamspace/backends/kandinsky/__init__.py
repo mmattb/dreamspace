6ecda862bc0ffd4823143bf7044b00b2d0de19aa
mmattb <mmattb@gmx.com>
2025-08-06 12:19:58 +0700

    repeat last

--- src/dreamspace/backends/kandinsky/__init__.py
+++ src/dreamspace/backends/kandinsky/__init__.py
@@ -1,6 +1,5 @@
 """Kandinsky backend package."""
 
-from .local_backend import LocalKandinskyBackend
 from .kandinsky21_server_backend import Kandinsky21ServerBackend
 
-__all__ = ["LocalKandinskyBackend", "Kandinsky21ServerBackend"]
+__all__ = ["Kandinsky21ServerBackend"]
